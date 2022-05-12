acdcd37f66b9eba29cc96ea895e433d503bee165
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-05-12 13:01:03 +0200

    bump

--- manual/src/main/java/hr/javacro/Main.java
+++ manual/src/main/java/hr/javacro/Main.java
@@ -1,5 +1,6 @@
 package hr.javacro;
 
+import io.sentry.Sentry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +8,10 @@ public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        Sentry.init(opts -> {
+            opts.setDsn("https://7561d36ce9ea4265abf4a77deb78af3a@o420886.ingest.sentry.io/6389805");
+            opts.setDebug(true);
+        });
         var conference = new Conference(1L, null);
 
         try {
@@ -14,6 +19,7 @@ public class Main {
             System.out.println(conference.name().toLowerCase());
             party(conference);
         } catch (Exception e) {
+            Sentry.captureException(e);
             LOGGER.error("Ooops", e);
         }
     }
