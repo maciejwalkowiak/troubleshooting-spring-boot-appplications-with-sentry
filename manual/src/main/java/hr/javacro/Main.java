c267de31de8f8ca570346e1102d5af98eb305439
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-05-13 10:09:49 +0200

    Fixes DEVOXX-CONSOLE-1

--- manual/src/main/java/hr/javacro/Main.java
+++ manual/src/main/java/hr/javacro/Main.java
@@ -1,8 +1,10 @@
 package hr.javacro;
 
+import io.sentry.HubAdapter;
 import io.sentry.Sentry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
@@ -11,18 +13,27 @@ public class Main {
         Sentry.init(opts -> {
             opts.setDsn("https://7561d36ce9ea4265abf4a77deb78af3a@o420886.ingest.sentry.io/6389805");
             opts.setDebug(true);
-            opts.setRelease("1.0");
+            opts.setRelease("1.2");
+            opts.addContextTag("userId");
         });
         var conference = new Conference(1L, null);
+
+        Sentry.configureScope(scope -> {
+            scope.setContexts("cli", "foo bar");
+        });
+
+        MDC.put("userId", "maciej");
 
         try {
             LOGGER.info("Hello JavaCro!!");
             System.out.println(conference.name().toLowerCase());
             party(conference);
+//            throw new IllegalStateException("foooobar!");
         } catch (Exception e) {
-            Sentry.captureException(e);
             LOGGER.error("Ooops", e);
         }
+
+
     }
 
     private static void party(Conference conference) {
